57022bd60e5d87f97d46beef5d1c9e85ca30f5ad
Yasunori Shimura <63567854+yas-sim@users.noreply.github.com>
2020-05-24 20:39:36 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,88 @@
+# Visualization of Facial Feature Vectors using T-SNE Algorithm
+The OpenVINO(tm) Open Model Zoo (OMZ) provides several facial feature extraction models. Those models take a cropped face image and generates a feature vector which consists of 256 FP32 values. Users can identify similar objects by calculating the norm (or distance) of those feature vectors.  
+This is a sample program to demonstrate how to visualize those 256-dimentional vectors on a chart.  
+This program uses t-SNE (T-distributed stochastic neighbor embedding) method to reduce the dimention of the vectors from 256 to 2, then plot the vectors on a chart.  
+You can understand that the chart shows the good separation of the feature vectors. The same persons faces are plotted close to each other and separated from the other faces.  
+
+OpenVINO(tm) Open Model Zooではいくつかの顔特徴ベクトル抽出モデルを提供しています。これらのモデルは切り抜かれた顔イメージを入力とし、256のFP32数値からなる特徴ベクトルを出力します。ユーザーはこの特徴ベクトル同士の距離を計算することでオブジェクトの同定を行うことが可能です。  
+ここではこの特徴ベクトルを図に表示する方法について説明しています。  
+このプログラムではt-SNE (T-distributed stochastic neighbor embedding)法を使用し、特徴ベクトルの次元数を256から2に圧縮し、図に表示しています。  
+図を見ると、特徴ベクトルがきれいに分離されていることがわかります。同じ人の顔のベクトルは近くに、そして他の人のベクトルからは離れた位置にプロットされています。
+
+**The faces found in the `./face-db` directory**
+![face_db](./resources/db.png)  
+
+**Scatter Chart of the face feature vectors obtained by `face-reidentification` model**
+![scatter_chart](./resources/t-sne.png)
+
+## Required DL Models to Run This Demo
+
+The demo expects the following models in the Intermediate Representation (IR) format:
+
+ * For face detection and re-identification 
+   * `face-detection-adas-0001`
+   * `face-reidentification-retail-0095`
+
+
+You can download those models from OpenVINO [Open Model Zoo](https://github.com/opencv/open_model_zoo).
+In the `models.lst` is the list of appropriate models for this demo that can be obtained via `Model downloader`.
+Please see more information about `Model downloader` [here](../../../tools/downloader/README.md).
+
+## How to Run
+
+(Assuming you have successfully installed and setup OpenVINO 2020.2. If you haven't, go to the OpenVINO web page and follow the [*Get Started*](https://software.intel.com/en-us/openvino-toolkit/documentation/get-started) guide to do it.)  
+
+### 1. Install dependencies  
+The demo depends on:
+- `opencv-python`
+- `numpy`
+- `scipy`
+- `scikit-learn`
+- `matplotlib`
+
+To install all the required Python modules you can use:
+
+``` sh
+(Linux) pip3 install -r requirements.txt
+(Win10) pip install -r requirements.txt
+```
+
+### 2. Download DL models from OMZ
+Use `Model Downloader` to download the required models.
+``` sh
+(Linux) python3 $INTEL_OPENVINO_DIR/deployment_tools/tools/model_downloader/downloader.py --list models.lst
+(Win10) python "%INTEL_OPENVINO_DIR%\deployment_tools\tools\model_downloader\downloader.py" --list models.lst
+```
+
+### 3. Prepare face images
+This program will search images (jpg/bmp/png) from `./face-db` directory. You can create the directories under `./face-db` as many as you want. The directory names and the file names are displayed right next to the dot on the chart so that you can identify which data it is.  
+
+#### Example of `face-db`
+```sh
+./
++ face-db/
+| + person0/
+| | + 0.jpg
+| | + 1.jpg
+| + person1/
+| | + 0.jpg
+| | + 1.jpg
+```
+
+### 4. Run the demo app
+This program doesn't take any command line arguments. All file names and paths are hard coded in the source code.
+``` sh
+(Linux) python3 face-feature-visualization.py
+(Win10) python face-feature-visualization.py
+```
+
+## Demo Output  
+The application displays the face database (found faces), then a chart.
+
+## Tested Environment  
+- Windows 10 x64 1909 and Ubuntu 18.04 LTS  
+- Intel(r) Distribution of OpenVINO(tm) toolkit 2020.2  
+- Python 3.6.5 x64  
+
+## See Also  
+* [Using Open Model Zoo demos](../../README.md)  
---
+++ face-feature-visualization.py
@@ -0,0 +1,136 @@
+import os
+import sys
+
+import cv2
+import numpy as np
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
+from openvino.inference_engine import IENetwork, IECore
+
+# DL models for face detection and re-identification
+model_det  = 'face-detection-adas-0001'             # data, Shape:[1, 3, 384, 672] / detection_out, Shape:[1, 1, 200, 7]
+model_reid = 'face-reidentification-retail-0095'    # 0, Shape:[1, 3, 128, 128] / 658, Shape:[1, 256, 1, 1]
+model_det  = 'intel/' + model_det  + '/FP16/' + model_det
+model_reid = 'intel/' + model_reid + '/FP16/' + model_reid
+
+def findFacesAndGenerateFeatures(img, ID, exec_det, net_det, exec_reid, net_reid):
+    input_name_det  = next(iter(net_det.inputs))
+    input_shape_det = net_det.inputs[input_name_det].shape
+    out_name_det    = next(iter(net_det.outputs))
+    out_shape_det   = net_det.outputs[out_name_det].shape
+
+    input_name_reid  = next(iter(net_reid.inputs))
+    input_shape_reid = net_reid.inputs[input_name_reid].shape
+    out_name_reid    = next(iter(net_reid.outputs))
+    out_shape_reid   = net_reid.outputs[out_name_reid].shape
+
+    inBlob = cv2.resize(img, (input_shape_det[3], input_shape_det[2]))
+    inBlob = inBlob.transpose((2,0,1))
+    res_det = exec_det.infer(inputs={input_name_det:inBlob})[out_name_det][0][0]
+    ret = []
+    idx = 0
+    for obj in res_det:
+        if obj[2]>0.6:
+            xmin = abs(int(obj[3] * img.shape[1]))
+            ymin = abs(int(obj[4] * img.shape[0]))
+            xmax = abs(int(obj[5] * img.shape[1]))
+            ymax = abs(int(obj[6] * img.shape[0]))
+            class_id = int(obj[1])
+            face = img[ymin:ymax,xmin:xmax].copy()
+            
+            inFace=cv2.resize(face, (input_shape_reid[3], input_shape_reid[2]))
+            inFace=inFace.transpose((2,0,1))
+            inFace=inFace.reshape(input_shape_reid)
+            res_reid = exec_reid.infer(inputs={input_name_reid: inFace})[out_name_reid]
+            res_reid = res_reid.reshape((256))
+
+            name = ID+str(idx)
+            idx+=1
+            ret.append({'name': name, 'img':face, 'feature': res_reid})
+    return ret
+
+def createDB(root_dir, exec_det, net_det, exec_reid, net_reid):
+    face_db = []
+    for dir in os.listdir(root_dir):
+        face_db.append([])
+        for file in os.listdir(os.path.join(root_dir, dir)):
+            if '.jpg' in file or '.png' in file or '.bmp' in file:
+                img = cv2.imread(os.path.join(root_dir, dir, file))
+                ID = os.path.join(dir, file)
+                faces = findFacesAndGenerateFeatures(img, ID, exec_det, net_det, exec_reid, net_reid)
+                face_db[-1]+=faces
+    return face_db
+
+def displayFacesInDB(face_db):
+    num_people = len(face_db)
+    max_faces=0
+    for person in face_db:
+        if len(person)>max_faces:
+            max_faces = len(person)
+    plt.figure()
+    for row, person in enumerate(face_db):
+        for col, face in enumerate(person):
+            plt.subplot(num_people, max_faces, row*max_faces+col+1)
+            img = cv2.cvtColor(face['img'], cv2.COLOR_BGR2RGB)
+            plt.imshow(img)
+            plt.title(face['name'], fontdict={'family':'TakaoGothic'})
+            plt.axis('off')
+    plt.show()
+
+def disp(reduced, label):
+    xmin = min(reduced[:,0])
+    xmax = max(reduced[:,0])
+    ymin = min(reduced[:,1])
+    ymax = max(reduced[:,1])
+    xymax = max([abs(xmin), abs(xmax), abs(ymin), abs(ymax)]) * 1.2
+    scale = (300)/xymax
+    img = np.full((300*2, 300*2, 3), (255,255,255), np.uint8)
+    for l, r in zip(label, reduced):
+        x = int(r[0]*scale)+300
+        y = int(r[1]*scale)+300
+        cv2.circle(img, (x,y), 4, (0,0,0), -1)
+        cv2.putText(img, l, (x,y), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 1)
+    cv2.imshow('scatter', img)
+    cv2.waitKey(1)
+
+def reduceDimensionAndDisplayScatterChart(face_db):
+    vec=[]
+    name=[]
+    for person in face_db:
+        for face in person:
+            vec.append(np.array(face['feature'], dtype=np.float32))
+            name.append(face['name'])
+
+    tsne = TSNE(n_components=2, init='random', random_state=0, perplexity=1)
+    reduced = tsne.fit_transform(vec)
+
+    #disp(reduced, name)
+
+    fig, ax = plt.subplots(figsize=(5,5))
+    cmap=plt.get_cmap('Dark2')
+    for i in range(reduced.shape[0]):
+        cval = cmap(i)
+        ax.scatter(reduced[i][0], reduced[i][1], marker='.', color=cval)
+        ax.annotate(name[i], xy=(reduced[i][0], reduced[i][1]), color=cval)
+    plt.show()
+
+
+def main():
+    ie = IECore()
+
+    # Prep for face detection
+    net_det  = ie.read_network(model_det+'.xml', model_det+'.bin')
+    exec_net_det    = ie.load_network(net_det, 'CPU')
+
+    # Preparation for face re-identification
+    net_reid = ie.read_network(model_reid+".xml", model_reid+".bin")
+    exec_net_reid    = ie.load_network(net_reid, 'CPU')
+
+    db = createDB('face-db', exec_net_det, net_det, exec_net_reid, net_reid)
+    displayFacesInDB(db)
+    reduceDimensionAndDisplayScatterChart(db)
+
+if __name__ == '__main__':
+        sys.exit(main() or 0)
